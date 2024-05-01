9ae035f464e327ea14adda0139d606bb1e1164ab
PustanAlex <alecsandru_bogdy@yahoo.com>
2024-05-01 17:29:16 +0300

    a

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let bodyColorChange;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    bodyColorChange = setInterval(function() {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000); 
+  });
+
+  stopButton.addEventListener('click', () => {
+    clearInterval(bodyColorChange);
+    startButton.disabled = false;
+  });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  
+  const formData = new FormData(event.target);
+  const firstDelay = parseInt(formData.get('delay'));
+  const delayStep = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const delay = firstDelay + (i - 1) * delayStep;
+
+    try {
+      const result = await createPromise(position, delay);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+    } catch (error) {
+      Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+    }
   }
 }
+
+document.querySelector('.form').addEventListener('submit', handleFormSubmit);
